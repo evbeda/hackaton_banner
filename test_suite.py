d86d8eaf0a9bcaa585b05febeeb2488d94d75e7d
Fernando Mez <fmezzabotta@eventbrite.com>
2018-03-20 09:39:06 -0300

    First commit

---
+++ test_suite.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+def suite():
+    """
+        Gather all the tests from this module in a test suite.
+    """
+    test_suite = unittest.TestSuite()
+    return test_suite
+
+
+if __name__ == "__main__":
+    unittest.main()
